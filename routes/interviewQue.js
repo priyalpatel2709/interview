8a5a6fddd0e36618d6a4cc46495b1e48f2db6ff2
priyalpatel2709 <piyu9552@gmail.com>
2024-09-23 13:55:08 +0530

    remove JWT

--- routes/interviewQue.js
+++ routes/interviewQue.js
@@ -17,10 +17,10 @@ const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/login").post(authUser);
-router.route("/").post(protect, addMockData);
-router.get("/", protect, getAllMockData);
-router.get("/:id", protect, getMockDataById);
-router.put("/:id", protect, updateMockData);
-router.delete("/:id", protect, deleteMockData);
+router.route("/").post(addMockData);
+router.get("/", getAllMockData);
+router.get("/:id", getMockDataById);
+router.put("/:id", updateMockData);
+router.delete("/:id", deleteMockData);
 
 module.exports = router;
